472cd374c31ab044f1fc1c3e10a60242b6d4bdd5
ThomasLemann <misham.morozov@gmail.com>
2025-11-13 20:58:34 +0200

    test

--- client.py
+++ client.py
@@ -15,8 +15,20 @@ else:
 
 responsetwo = requests.get('http://127.0.0.1:8080/user/usrnm')
 
-print(responsetwo.text)
+if responsetwo.status_code != 200:
+    print(f"{ERR}Error occured{RST}")
+    print(responsetwo.text)
+else:
+    print(f"{OK} Server is working correctly{RST}")
+    print("::notice title=Healthcheck passed::Responce is adequate")
+    print(responsetwo.text)
 
 responsethree = requests.get('http://127.0.0.1:8080/search?q=python&length=17')
 
-print(responsethree.text)
+if responsethree.status_code != 200:
+    print(f"{ERR}Error occured{RST}")
+    print(responsethree.text)
+else:
+    print(f"{OK} Server is working correctly{RST}")
+    print("::notice title=Healthcheck passed::Responce is adequate")
+    print(responsethree.text)
